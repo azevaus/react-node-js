7d944c587a094398112b0bb6d3bb4818e359c913
azevaus <andressa@ip3tubo.com>
2021-09-20 09:04:53 -0300

    master

---
+++ frontend/src/components/Slider.js
@@ -0,0 +1,26 @@
+import React from "react";
+import SimpleImageSlider from "react-simple-image-slider";
+const images = [
+    { url: "images/1.jpg" },
+    { url: "images/2.jpg" },
+    { url: "images/3.jpg" },
+    { url: "images/4.jpg" },
+    { url: "images/5.jpg" },
+    { url: "images/6.jpg" },
+    { url: "images/7.jpg" },
+  ];
+
+const Slider = () =>{
+    return(
+        <div>
+            <SimpleImageSlider
+                width={1720}
+                height={740}
+                images={images}
+                showBullets={true}
+                showNavs={true}
+            />
+        </div>
+    )
+}
+export default Slider
--- frontend/src/pages/OnePage.js
+++ frontend/src/pages/OnePage.js
@@ -3,10 +3,12 @@ import BudgetSection from '../components/BudgetSection';
 import NewsSection from '../components/NewsSection';
 import PartnershipsSection from '../components/PartnershipsSection';
 import ProductsSection from '../components/ProductsSection';
+import Slider from '../components/Slider';
 import SpecialistSection from '../components/SpecialistSection';
 const OnePage = () => {
     return (
         <div>
+            <Slider/>
             <ProductsSection/>
             <NewsSection/>
             <SpecialistSection/>
